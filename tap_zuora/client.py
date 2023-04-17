e005c761ce0e42f22f152ad2f184c99612f2e93a
Jake Kirsch <jake.kirsch@pantheon.io>
2023-04-17 09:58:00 -0500

    adding oauth

--- tap_zuora/client.py
+++ tap_zuora/client.py
@@ -1,6 +1,17 @@
+from typing import Dict, Tuple
+
+import backoff
 import requests
 import singer
 from singer import metrics
+
+from tap_zuora.exceptions import (
+    ApiException,
+    BadCredentialsException,
+    RateLimitException,
+    RetryableException,
+)
+from tap_zuora.utils import make_aqua_payload
 
 IS_AQUA = False
 IS_REST = True
@@ -10,14 +21,13 @@ NOT_EURO = False
 IS_EURO = True
 
 URLS = {
-    (IS_AQUA, IS_PROD, NOT_EURO): "https://www.zuora.com/",
-    (IS_AQUA, IS_SAND, NOT_EURO): "https://apisandbox.zuora.com/",
-    (IS_AQUA, IS_PROD, IS_EURO): "https://rest.eu.zuora.com/",
-    (IS_AQUA, IS_SAND, IS_EURO): "https://rest.sandbox.eu.zuora.com/",
-    (IS_REST, IS_PROD, NOT_EURO): "https://rest.zuora.com/",
-    (IS_REST, IS_SAND, NOT_EURO): "https://rest.apisandbox.zuora.com/",
-    (IS_REST, IS_PROD, IS_EURO): "https://rest.eu.zuora.com/",
-    (IS_REST, IS_SAND, IS_EURO): "https://rest.sandbox.eu.zuora.com/",
+    (IS_PROD, NOT_EURO): ["https://rest.na.zuora.com/", "https://rest.zuora.com/"],
+    (IS_SAND, NOT_EURO): [
+        "https://rest.sandbox.na.zuora.com/",
+        "https://rest.apisandbox.zuora.com/",
+    ],
+    (IS_PROD, IS_EURO): ["https://rest.eu.zuora.com/"],
+    (IS_SAND, IS_EURO): ["https://rest.sandbox.eu.zuora.com/"],
 }
 
 LATEST_WSDL_VERSION = "91.0"
@@ -25,64 +35,195 @@ LATEST_WSDL_VERSION = "91.0"
 LOGGER = singer.get_logger()
 
 
-class ApiException(Exception):
-    def __init__(self, resp):
-        self.resp = resp
-        # pylint: disable=super-with-arguments
-        super(ApiException, self).__init__("{0.status_code}: {0.content}".format(self.resp))
-
-
-# pylint: disable=too-many-arguments
-class Client:
-    def __init__(self, username, password, partner_id, sandbox=False, european=False):
+class Client:  # pylint: disable=too-many-instance-attributes
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        partner_id: str,
+        sandbox: bool = False,
+        european: bool = False,
+        is_rest: bool = False,
+        oauth: bool = False,
+    ):
         self.username = username
         self.password = password
         self.sandbox = sandbox
         self.european = european
         self.partner_id = partner_id
+        self.is_rest = is_rest
+        self.token = None
         self._session = requests.Session()
 
-        adapter = requests.adapters.HTTPAdapter(max_retries=1) # Try again in the case the TCP socket closes
-        self._session.mount('https://', adapter)
+        if oauth:
+            url = self.get_url("oauth/token", rest=True)
+
+            resp = self._request(
+                "POST",
+                url,
+                headers={
+                    "content-type": "application/x-www-form-urlencoded",
+                },
+                data={
+                    "grant_type": "client_credentials",
+                    "client_id": self.username,
+                    "client_secret": self.password,
+                },
+            )
+            self.token = resp.json()["access_token"]
+
+        self.base_url = self.get_url()
+
+        adapter = requests.adapters.HTTPAdapter(max_retries=5)  # Try again in the case the TCP socket closes
+        self._session.mount("https://", adapter)
 
     @staticmethod
-    def from_config(config):
-        sandbox = config.get('sandbox', False) == 'true'
-        european = config.get('european', False) == 'true'
-        partner_id = config.get('partner_id', None)
-        return Client(config['username'], config['password'], partner_id, sandbox, european)
+    def from_config(config: Dict):
+        sandbox = config.get("sandbox", False) == "true"
+        european = config.get("european", False) == "true"
+        partner_id = config.get("partner_id", None)
+        is_rest = config.get("api_type") == "REST"
+        oauth = config.get("oauth", False) == "true"
+        return Client(
+            config["username"],
+            config["password"],
+            partner_id,
+            sandbox,
+            european,
+            is_rest,
+            oauth,
+        )
 
-    def get_url(self, url, rest=False):
-        return URLS[(rest, self.sandbox, self.european)] + url
+    def get_url(self) -> str:
+        """gets the base_url from potential_urls based on configurations."""
+        potential_urls = URLS[(self.sandbox, self.european)]
+        stream_name = "Account"
+        for url_prefix in potential_urls:
+            if self.is_rest:
+                resp = self._retryable_request(
+                    "GET",
+                    f"{url_prefix}v1/describe/{stream_name}",
+                    url_check=True,
+                    headers=self.rest_headers,
+                )
+
+            else:
+                query = f"select * from {stream_name} limit 1"
+                post_url = f"{url_prefix}v1/batch-query/"
+                payload = make_aqua_payload("discover", query, self.partner_id)
+                resp = self._retryable_request("POST", post_url, url_check=True, auth=self.aqua_auth, json=payload)
+                if resp.status_code == 200:
+                    resp_json = resp.json()
+                    if "errorCode" in resp_json:
+                        # Zuora sends 200 status code for an unrecognized partner ID in AQuA calls.
+                        raise Exception(
+                            resp_json.get(
+                                "message",
+                                "Partner ID is not recognized."
+                                " To obtain a partner ID,"
+                                " submit a request with Zuora Global Support",
+                            )
+                        )
+
+                    delete_id = resp_json["id"]
+                    delete_url = f"{url_prefix}v1/batch-query/jobs/{delete_id}"
+                    self._retryable_request("DELETE", delete_url, auth=self.aqua_auth)
+            if resp.status_code == 401:
+                continue
+            else:
+                return url_prefix
+        raise BadCredentialsException(
+            f'Could not discover {"EU-based" if self.european else "US-based"} '
+            f'{"REST" if self.is_rest else "AQuA"} '
+            f'{"Sandbox" if self.sandbox else "Production"} '
+            f"data center url out of {potential_urls} "
+            f"for provided credentials."
+        )
 
     @property
-    def aqua_auth(self):
-        return (self.username, self.password)
+    def aqua_auth(self) -> Tuple:
+        return self.username, self.password
 
     @property
-    def rest_headers(self):
+    def rest_headers(self) -> Dict:
+        """Returns headers for HTTP request."""
+        if self.token:
+            return {
+                "Content-Type": "application/json",
+                "authorization": "Bearer {}".format(
+                    self.token
+                ),
+            }
         return {
-            'apiAccessKeyId': self.username,
-            'apiSecretAccessKey': self.password,
-            'X-Zuora-WSDL-Version': LATEST_WSDL_VERSION,
-            'Content-Type': 'application/json',
+            "apiAccessKeyId": self.username,
+            "apiSecretAccessKey": self.password,
+            "X-Zuora-WSDL-Version": LATEST_WSDL_VERSION,
+            "Content-Type": "application/json",
         }
 
-    def _request(self, method, url, stream=False, **kwargs):
+    # NB> Backoff as recommended by Zuora here:
+    # https://community.zuora.com/t5/Release-Notifications/Upcoming-Change-for-AQuA-and-Data-Source-Export-January-2021/ba-p/35024
+    @backoff.on_exception(
+        backoff.expo,
+        (RateLimitException, RetryableException),
+        max_tries=5,
+        factor=30,
+        jitter=None,
+    )
+    def _retryable_request(self, method: str, url: str, stream=False, url_check=False, **kwargs) -> requests.Response:
+        """
+        Performs HTTP request
+        Retries the request for 5 times upon encountering exception
+        Args:
+            method (str): HTTP Method type
+            url (str): API base_url + endpoint
+        """
         req = requests.Request(method, url, **kwargs).prepare()
-        LOGGER.info("%s: %s", method, req.url)
         resp = self._session.send(req, stream=stream)
+
+        if resp.status_code == 429:
+            raise RateLimitException(resp)
+        # retries the request when response is either 500(Internal Server Error)
+        # 502(Bad Gateway), 503(service unavailable), 504(Gateway Timeout)
+        if resp.status_code in [500, 502, 503, 504]:
+            raise RetryableException(resp)
+        self.check_for_error(resp, url_check)
+        return resp
+
+    @staticmethod
+    def check_for_error(resp, url_check):
+        """
+        Args:
+            url_check [boolean]: checks if api call is for validating the domain url
+            resp [requests.Response]: HTTP Response object
+        """
+        # If condition skip raising 400 exception when we test for stream availability
+        # When some Stream is not available then api returns a 400 error with message noSuchDataSource
+        if (
+            not url_check
+            and resp.status_code == 400
+            and "noSuchDataSource" in resp.json().get("Errors", [{"Message": ""}])[0]["Message"]
+        ):
+            return
+
+        if not url_check:
+            resp.raise_for_status()
+
+    def _request(self, method: str, url: str, **kwargs) -> requests.Response:
+        LOGGER.info(f"{method}: {url}")
+        resp = self._retryable_request(method, url, **kwargs)
+
         if resp.status_code != 200:
             raise ApiException(resp)
 
         return resp
 
-    def aqua_request(self, method, url, **kwargs):
-        with metrics.http_request_timer(url):
-            url = self.get_url(url, rest=False)
+    def aqua_request(self, method: str, path: str, **kwargs) -> requests.Response:
+        with metrics.http_request_timer(path):
+            url = self.base_url + path
             return self._request(method, url, auth=self.aqua_auth, **kwargs)
 
-    def rest_request(self, method, url, **kwargs):
-        with metrics.http_request_timer(url):
-            url = self.get_url(url, rest=True)
+    def rest_request(self, method: str, path: str, **kwargs) -> requests.Response:
+        with metrics.http_request_timer(path):
+            url = self.base_url + path
             return self._request(method, url, headers=self.rest_headers, **kwargs)
